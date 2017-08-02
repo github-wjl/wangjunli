b77efec82c35f15f5eeeda7b97dce33180c2075f
github-wjl <wangjunli0909>
2017-08-02 15:45:02 +0800

    第一次提交

--- app/src/main/java/com/example/administrator/wangjunli/MainActivity.java
+++ app/src/main/java/com/example/administrator/wangjunli/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.administrator.wangjunli;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loaddata();
+
     }
+
+    private void loaddata() {
+        textView= (TextView) findViewById(R.id.text);
+        textView.setText("第一次提交");
+    }
+
 }
